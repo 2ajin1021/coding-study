a4a93c12e7da76afe67eda8c4c6d28ebee5fe07f
2ajin1021 <2ajinny1021@gmail.com>
2025-04-28 15:01:15 +0900

    [Bronze III] Title: 세 수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.py
+++ 백준/Bronze/10817. 세 수/세 수.py
@@ -1,3 +1,11 @@
 num = list(map(int, input().split()))
-num.sort()
-print(num[1])
+A = num[0]
+B = num[1]
+C = num[2]
+
+if (A >= B and A <= C) or (A >= C and A <= B):
+    print(A)
+elif (B >= A and B <= C) or (B >= C and B <= A):
+    print(B)
+else:
+    print(C)
